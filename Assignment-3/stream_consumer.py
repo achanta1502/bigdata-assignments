3a5657006a61d760798bcc44bc916c8b83af4ace
achanta1502 <achanta_pavankumar@hotmail.com>
2019-11-05 01:42:06 -0600

    model built

--- Assignment-3/stream_consumer.py
+++ Assignment-3/stream_consumer.py
@@ -2,58 +2,112 @@ from kafka import KafkaConsumer
 import json, sys
 import os, re
 import pandas as pd
+import nltk
+import numpy as np
 from nltk.corpus import stopwords
 from nltk.tokenize import word_tokenize
 from nltk.stem.porter import PorterStemmer
 import csv
+from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn import model_selection
+from sklearn.naive_bayes import MultinomialNB
+from sklearn.metrics import accuracy_score
 
-def data_from_kafka_consumer(topic, time):
+def data_from_kafka_consumer(topic):
     data = []
+    df = pd.DataFrame()
     consumer = KafkaConsumer(topic,
                              bootstrap_servers=['localhost:9092'],
                              auto_offset_reset='earliest'
                              )
+    time = 0
     for msg in consumer:
-        if time <= 0:
+        if time > 100:
             break
-        cols = msg.value.split("||")
-        cols[-1] = re.sub('[^A-Za-z0-9\s]+', '', cols[-1])
-        data.append(cols)
-        time = time - 1
+        decoded = msg.value.decode('utf-8')
+        cols  = decoded.split("||")
+        # dec = re.sub('[^A-Za-z0-9\s]+', ' ', cols[1])
+        asciidata = cols[1].encode("ascii", "ignore")
+        data.append([asciidata, cols[0]])
+        time = time + 1
+        # vals = data.get(cols[0], [])
+        # vals.append(re.sub('[^A-Za-z0-9\s]+', ' ', cols[-1]))
+        # data[cols[0]] = vals
+    # print(word_tokenize(data[0][0]))
+    # df.columns = ['headline', 'label']
     #df = pd.DataFrame(data)
-    with open('/tmp/output.csv', 'wb') as test_file:
-        file_writer = csv.writer(test_file)
-        file_writer.writerows(data)
-    print(data)
-    return data
+    with open('/tmp/output.csv', mode='w') as test_file:
+        writer = csv.writer(test_file)
+        for line in data:
+            writer.writerow(line)
+    return df
+
+def data_from_text():
+    df = pd.read_csv("/tmp/output.csv", delimiter=',', header=None)
+    df.columns = ['review', 'label']
+    return df
 
 def stopwords_removal():
     return set(stopwords.words('english'))
 
-def tokenize(sentences):
+def tokenize(text):
+    tokens = set(word_tokenize(text))
     stop_words = stopwords_removal()
-    word_tokens = []
-    for sentence in sentences:
-        word_tokens.extend(word_tokenize(sentence))
-    remove_stopwords = set(w for w in word_tokens if not w in stop_words)
-    return sorted(list(remove_stopwords))
+    tokens = [w for w in tokens if not w in stop_words]
+    stems = [porter(item) for item in tokens]
+    return stems
 
 def porter(word):
     return PorterStemmer().stem(word)
 
-def stemmer(tokenized_words):
-    for i in range(len(tokenized_words)):
-        tokenized_words[i] = porter(tokenized_words[i])
-    return tokenized_words
 
-def text_processing(sentences):
-    words = tokenize(sentences)
+def vector_fit_transform(X_train, X_test):
+    vect = TfidfVectorizer(tokenizer=tokenize, use_idf=True)
+    train_vectors = vect.fit_transform(X_train)
+    test_vectors = vect.transform(X_test)
+    return (train_vectors, test_vectors)
+
+def text_processing():
+    df = data_from_text()
+    return df
+
+def train_test(df):
+    X_train = df.loc[:80, 'review'].values
+    Y_train = df.loc[:80, 'label'].values
+    X_test = df.loc[80:, 'review'].values
+    Y_test = df.loc[80:, 'label'].values
+    return X_train, Y_train, X_test, Y_test
+
+def model(X_train, y_train):
+    clf = MultinomialNB().fit(X_train, y_train)
+    return clf
+
+def predict(model, test):
+    return model.predict(test)
+
+def model_building():
+    df = text_processing()
+    X_train, Y_train, X_test, Y_test = train_test(df)
+    train_vectors, test_vectors = vector_fit_transform(X_train, X_test)
+    clf = model(train_vectors, Y_train)
+    predicted = predict(clf, test_vectors)
+    print(accuracy_score(predicted, Y_test))
 
 
 if __name__ == "__main__":
-    if len(sys.argv) != 3:
-        print("not enough arguments")
-        exit()
-    topic = sys.argv[1]
-    time = int(sys.argv[2])
-    sentence = data_from_kafka_consumer(topic, time)
+    # if len(sys.argv) != 2:
+    #     print("not enough arguments")
+    #     exit()
+    # topic = sys.argv[1]
+    # # time = int(sys.argv[2])
+    # mapped_data = data_from_kafka_consumer(topic)
+    # print(df)
+    # X_train, X_validation, y_train, y_validation = train_test(mapped_data)
+    # model = model(X_train, y_train)
+   #     predicted_value = predict(model, X_validation)
+    # for i in range(len(predicted_value)):
+    #     print(y_validation[i], predicted_value[i])
+    model_building()
+
+
